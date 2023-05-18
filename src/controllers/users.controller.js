fd4c1a1ac6b052c797fa0cfa21184997a4c162b7
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-17 22:12:50 -0300

    Feat: get user info

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,32 @@
 import { db } from "../database/database.connection.js";
 
 export async function userInfo(req, res) {
+  const { id: userId } = res.locals.user;
   try {
-    res.send("placeholder");
+    const query = `
+      SELECT 
+      users.id, 
+      users.name, 
+      COALESCE ( SUM ( links.visits ) , 0 ) AS "visitCount",
+      CASE
+        WHEN COUNT( links.id ) > 0
+          THEN JSONB_AGG(JSONB_BUILD_OBJECT(
+            'id', links.id,
+            'shortUrl', links."shortUrl",
+            'url', links.url,
+            'visitCount', links.visits
+          ))
+        ELSE '[]'
+      END AS "shortenedUrls"
+      FROM users
+        LEFT JOIN links ON users.id = links."userId"
+      WHERE users.id=$1
+      GROUP BY users.id, users.name;
+    `;
+    const { rows } = await db.query(query, [userId]);
+    const userLinks = rows[0];
+
+    res.send(userLinks);
   } catch (error) {
     res.status(500).send(error.message);
   }
